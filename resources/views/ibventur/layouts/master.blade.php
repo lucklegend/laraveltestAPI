4470f3f803db69cbe955951a15d5486a8abc39b9
Janno E. Nuyles <lucklegend@gmail.com>
2022-01-18 14:34:05 +0800

    kinulang pa. ahah

--- resources/views/ibventur/layouts/master.blade.php
+++ resources/views/ibventur/layouts/master.blade.php
@@ -93,17 +93,17 @@
         </div>
       </div>
     </div>
-    <footer class="footer-section align-center">
-      <div class="container">
-         <p>Copyright ©
-            <script>
-               var d = new Date();
-               document.write(d.getFullYear());
-            </script>
-            2022 By Iberian Ventures.</p>
-      </div>
-    </footer>
   </section>
+  <footer class="footer-section align-center">
+    <div class="container">
+       <p>Copyright ©
+          <script>
+             var d = new Date();
+             document.write(d.getFullYear());
+          </script>
+          2022 By Iberian Ventures.</p>
+    </div>
+  </footer>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="fontawesome/js/all.min.js"></script>
